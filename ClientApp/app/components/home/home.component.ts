b10effdcc24c208f83d353a1064ca41805a3a0b8
FreddyMcray <evgenuypetrushevski@gmail.com>
2017-09-14 21:46:57 +0300

    home component test

--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+    projec: UserProject = {
+        id: 1, urlImage: 'http://res.cloudinary.com/profunding/image/upload/v1504950919/default-bg.jpg',
+        title: 'Sasay project', description: 'This is sasay project. So, you need to sasay', currentSum: '200', leftOver: '40'
+    };
+    projects: UserProject[] = [this.projec, this.projec, this.projec, this.projec];
+}
+
+class UserProject {
+    id: number;
+    urlImage: string;
+    title: string;
+    description: string;
+    currentSum: string;
+    leftOver: string;
 }
